f2ecb92fd20fdc783c0f59a41c5eb17e1af305aa
Anirudh99999 <mudhulka@csp.edu>
2023-11-30 08:59:30 -0600

    git-test

---
+++ Remove-RG.ps1
@@ -0,0 +1,11 @@
+﻿# Set the resource group name
+$resourceGroupName = 'RG'
+
+# Check if the resource group exists
+if (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue) {
+    # Delete the resource group
+    Remove-AzResourceGroup -Name $resourceGroupName -Force -Verbose
+    Write-Host "Resource group '$resourceGroupName' deleted successfully."
+} else {
+    Write-Host "Resource group '$resourceGroupName' not found."
+}
